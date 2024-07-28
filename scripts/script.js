69796b2253bc73ad0bc235abbef3338f629928ae
J4nYa-GliTch <j4nya.cat@gmail.com>
2024-07-28 11:15:58 +0530

    Add result and moves

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,27 @@
-function game() {
-  const computerMove = computerMove();
+document.querySelector(".move-rock").addEventListener("click", () => {
+  game("rock");
+});
+document.querySelector(".move-paper").addEventListener("click", () => {
+  game("paper");
+});
+document.querySelector(".move-scissors").addEventListener("click", () => {
+  game("scissors");
+});
+
+function pickComputerMove() {
+  let computerMove = Math.random();
+  if (computerMove >= 0 && computerMove < 1 / 3) {
+    computerMove = "rock";
+  } else if (computerMove >= 1 / 3 && computerMove < 2 / 3) {
+    computerMove = "paper";
+  } else {
+    computerMove = "scissors";
+  }
+  return computerMove;
+}
+
+function game(playerMove) {
+  const computerMove = pickComputerMove();
   let result;
   if (computerMove === "rock") {
     if (playerMove === "rock") {
@@ -26,17 +48,9 @@ function game() {
       result = "Tie";
     }
   }
+  document.querySelector(".result").innerHTML = result;
+  document.querySelector(
+    ".game-moves"
+  ).innerHTML = `Player <img src="images/${playerMove}-emoji.png"> || <img src="images/${computerMove}-emoji.png"> Computer`;
   return result;
-}
-
-function computerMove() {
-  let computerMove = Math.random();
-  if (computerMove >= 0 && computerMove < 1 / 3) {
-    computerMove = "rock";
-  } else if (computerMove >= 1 / 3 && computerMove < 2 / 3) {
-    computerMove = "paper";
-  } else {
-    computerMove = "scissors";
-  }
-  return computerMove;
 }
